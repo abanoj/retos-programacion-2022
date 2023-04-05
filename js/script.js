e5bcb1057b29af51df84465635f49b3c0db931d4
abanoj <jaabanof@gmail.com>
2023-04-05 20:21:30 +0200

    Reto 2

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 
-const retoUno = () =>{
+/* const retoUno = () =>{
     for (let i = 1; i <= 100; i++){
         if(i%3 === 0 && i%5 === 0){ console.log('fizzbuzz'); continue;}
         if(i%3 === 0) {console.log('fizz'); continue;}
@@ -15,4 +15,38 @@ const retoUno = () =>{
         console.log(i);       
     }
 }
-retoUno();
+retoUno(); */
+
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+const retoDos = (firstWord = '', secondWord = '') => {
+    //Compruebo que existan las dos palabras y que sean tipo string
+    if (firstWord === '' || secondWord === '') return console.info('Falta un argumento en la función');
+    if (typeof firstWord !== 'string' || typeof secondWord !== 'string') return console.error('Los argumentos deben ser cadenas de texto');
+
+    firstWord = firstWord.toLowerCase();
+    secondWord = secondWord.toLowerCase();
+    //Comrpuebo si las palabras son exactamente iguales
+    if (firstWord === secondWord) return false;
+
+    firstWord = firstWord.split('').sort().join('');
+    secondWord = secondWord.split('').sort().join('');
+    //Comprueba si son palindromos
+    if (firstWord === secondWord){ return true}
+    return false;
+
+    console.log('Probando');
+}
+retoDos()
+retoDos(34,true);
+console.log(retoDos('Hola','Adios'));
+console.log(retoDos('Hola','hola'));
+console.log(retoDos('Aloha', 'Aolah'));
+
